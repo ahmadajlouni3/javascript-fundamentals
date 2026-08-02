f570b71f17030d3a313ee7b370afe6197f5cc581
ahmadajlouni3 <bootsetup1@gmail.com>
2026-08-02 11:56:13 +0300

    Solution 6

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 process.stdin.on("data", (chunk) => {    
     const data = chunk.toString().split("\n");
-    const name = data[0];
-    const age = data[1];
+    
 
-    console.log(`Hi, ${name}! You are ${age} years old.`)
+    if (data[0] % 3 === 0 && data[0] % 5 === 0) {
+        console.log("FizzBuzz")
+    } else if (data[0] % 3 === 0) {
+        console.log("Fizz");
+    } else if (data[0] % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(data[0]);
+    }
 })
