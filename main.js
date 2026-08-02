b6dc002069a0a6bb7bba0307588f36f41d424303
ahmadajlouni3 <bootsetup1@gmail.com>
2026-08-02 13:58:26 +0300

    Solution 13

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 process.stdin.on("data", (chunk) => {    
-    const data = chunk.toString().trim().split(" ");
-    
-    const result = data
-    .map(item => Number(item))
-    .filter(item => item % 2 === 0)
-    .map(item => Math.pow(item, 2))
-    .reduce((acc, item) => acc + item, 0);
+    const data = chunk.toString().trim().split(" ").map(item => Number(item));
+    function sum(...data) {
+        return data.flat().reduce((accumelator, item) => accumelator + item, 0);
+    }
 
-    console.log(result);
+    console.log(sum(data));
 })
