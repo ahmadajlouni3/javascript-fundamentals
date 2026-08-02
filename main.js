fa18cc16501281fdb91dede0089acf92f3a61114
ahmadajlouni3 <bootsetup1@gmail.com>
2026-08-02 14:12:28 +0300

    Solution 15

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-process.stdin.on("data", (chunk) => {    
-    const data = chunk.toString().trim().split(" ").map(item => Number(item));
-    
-
-    console.log(...data.filter(item => Number.isInteger(item) && item > 0));
-
-})
+yes
+default
+barrel
