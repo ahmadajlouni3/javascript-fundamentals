19bb88712e74eab764aaa9e1f616d5d5771bedee
ahmadajlouni3 <bootsetup1@gmail.com>
2026-08-02 12:06:21 +0300

    Solution 7

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 process.stdin.on("data", (chunk) => {    
-    const data = chunk.toString().split("\n");
+    const data = chunk.toString();
     
+    let num = Number(data);
+    let sum = 0;
+    let i = 0;
 
-    if (data[0] % 3 === 0 && data[0] % 5 === 0) {
-        console.log("FizzBuzz")
-    } else if (data[0] % 3 === 0) {
-        console.log("Fizz");
-    } else if (data[0] % 5 === 0) {
-        console.log("Buzz");
-    } else {
-        console.log(data[0]);
+    while (i < Number(data)) {
+        sum += num;
+        num--;
+        i++;
     }
+
+    console.log(sum)
+    
 })
