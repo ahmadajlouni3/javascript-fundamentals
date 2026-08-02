0b15be608a3c4f3b2dae8af4d61e5fb0811ac830
ahmadajlouni3 <bootsetup1@gmail.com>
2026-08-02 14:04:29 +0300

    Solution 14

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 process.stdin.on("data", (chunk) => {    
     const data = chunk.toString().trim().split(" ").map(item => Number(item));
-    function sum(...data) {
-        return data.flat().reduce((accumelator, item) => accumelator + item, 0);
-    }
+    
 
-    console.log(sum(data));
+    console.log(...data.filter(item => Number.isInteger(item) && item > 0));
+
 })
